eacf0f3b1c8dd7f48c8af0865aa5d503ee964dc1
Artyom <ptukhovAN@gmail.com>
2023-08-03 14:32:54 +0500

    fix: routes

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,7 +1,6 @@
 import {
   createBrowserRouter,
   createRoutesFromElements,
-  Navigate,
   Route,
 } from "react-router-dom";
 import { Upload } from "@/features/upload-page";
@@ -16,10 +15,10 @@ export enum RouteNames {
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path={RouteNames.START_PAGE} element={<App />}>
-      <Route
-        path={RouteNames.START_PAGE}
-        element={<Navigate to={RouteNames.PAGE} />}
-      />
+      {/*<Route*/}
+      {/*  path={RouteNames.START_PAGE}*/}
+      {/*  element={<Navigate to={RouteNames.PAGE} />}*/}
+      {/*/>*/}
       <Route path={RouteNames.PAGE} element={<Upload />} />
       <Route path={RouteNames.REDIRECT} element={<Upload />} />
     </Route>
